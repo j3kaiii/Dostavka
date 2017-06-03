de2ae57888cbfbe9c9160466692b1a5356a7f7e3
j3kaiii <j3kaiii@gmail.com>
2017-06-03 13:56:19 +0300

    start

---
+++ Dostavka/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 03.06.2017
+ * Time: 11:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Dostavka
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(75, 61);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(152, 29);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Дата доставки";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(259, 61);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(158, 29);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = "Номер расходки";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox3.Location = new System.Drawing.Point(75, 107);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(342, 29);
+			this.textBox3.TabIndex = 2;
+			this.textBox3.Text = "Наименование товара";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox4.Location = new System.Drawing.Point(75, 156);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(342, 29);
+			this.textBox4.TabIndex = 3;
+			this.textBox4.Text = "ФИО клиента";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox5.Location = new System.Drawing.Point(75, 204);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(342, 29);
+			this.textBox5.TabIndex = 4;
+			this.textBox5.Text = "Номер телефона";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox6.Location = new System.Drawing.Point(75, 258);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(342, 29);
+			this.textBox6.TabIndex = 5;
+			this.textBox6.Text = "Адрес доставки";
+			// 
+			// button1
+			// 
+			this.button1.Image = global::Dostavka.Resource1.set_icon1;
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(32, 32);
+			this.button1.TabIndex = 6;
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(194, 308);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(85, 38);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Печать";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(485, 364);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "Dostavka";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Dostavka/MainForm.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 03.06.2017
+ * Time: 11:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace Dostavka
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
